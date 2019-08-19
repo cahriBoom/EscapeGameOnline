ebd21ea88dbfe6bf05314888a4e608c78300fdea
cahriBoom <52654757+cahriBoom@users.noreply.github.com>
2019-08-19 16:59:52 +0200

    Maj Finale
    Log:
    Add traitement d'erreur non numérique

--- src/Human.java
+++ src/Human.java
@@ -3,33 +3,61 @@ import java.util.Scanner;
 public class Human extends Player {
 
     // Ask the player his response for cracking the code
-    public String askPlayer(int nb) {
+    public String askPlayer(String question, boolean modeIntegers) {
+        //Scanner sc = new Scanner(System.in);
+
         Scanner sc = new Scanner(System.in);
-        String choosenNumbers = sc.nextLine();
-        return choosenNumbers;
+        if (modeIntegers) {
+            Integer enteredIntegers;
+            while (true) {
+                System.out.print(question);
+                try {
+                    enteredIntegers = sc.nextInt();
+                    return enteredIntegers.toString();
+                } catch (java.util.NoSuchElementException e) {
+                    sc.nextLine();
+                    System.out.println("The input is not an integer");
+                }
+            }
+        } else {
+            String enteredComparators;
+            while (true) {
+                System.out.print(question);
+                enteredComparators = sc.nextLine();
+                boolean ok = true;
+                for (int i=0; i<enteredComparators.length();i++){
+                    if ((enteredComparators.charAt(i)!='=') && (enteredComparators.charAt(i)!='-') && (enteredComparators.charAt(i)!='+')){
+                        ok = false;
+                    }
+                }
+                if (ok) {
+                    return enteredComparators;
+                } else {
+//                    sc.nextLine();
+                    System.out.println("The string is not correct");
+                }
+            }
+        }
     }
+
     // Ask the player to type a code
     public String askPlayerNewCode(int nb) {
-        System.out.print("Proposition du joueur : ");
-        String chaine = this.askPlayer(nb);
+        String chaine = this.askPlayer("Proposition du joueur : ", true);
         if(chaine.length()!=nb){
             while(chaine.length()!=nb){
                 System.out.println("You may choose only " + nb + " Numbers");
-                System.out.print("Proposition du joueur : ");
-                chaine = this.askPlayer(nb);
+                chaine = this.askPlayer("Proposition du joueur : ", true);
             }
         }
         return chaine;
     }
     // Ask the player to type a response to the computer
     public String askPlayerSymbols(int nb) {
-        System.out.print("Réponse du joueur : ");
-        String chaine =  this.askPlayer(nb);
+        String chaine =  this.askPlayer("Réponse du joueur : ", false);
         if(chaine.length()!=nb){
             while(chaine.length()!=nb){
                 System.out.println("You may choose only " + nb + " symbols");
-                System.out.print("Reponse du joueur : ");
-                chaine = this.askPlayer(nb);
+                chaine = this.askPlayer("Réponse du joueur : ", false);
             }
         }
         return chaine;
@@ -37,15 +65,14 @@ public class Human extends Player {
     // Ask the player to type his/her secret combination
     public void askPlayerSecretCombinaison(int nb) {
         String secretCombinaison;
-        System.out.print("Combinaison secrète du joueur : ");
-        secretCombinaison = this.askPlayer(nb);
+        secretCombinaison = this.askPlayer("Combinaison secrète du joueur : ", true);
         if(secretCombinaison.length()!=nb){
             while(secretCombinaison.length()!=nb){
                 System.out.println("You may choose only " + nb + " Numbers");
-                System.out.print("Combinaison secrète du joueur : ");
-                secretCombinaison = this.askPlayer(nb);
+                secretCombinaison = this.askPlayer("Combinaison secrète du joueur : ", true);
             }
         }
         this.setCombinaison(secretCombinaison);
     }
 }
+
